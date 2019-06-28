6d55162e1851ab7273d3f3dca06b4694d5ba8193
kyle-luo <tsjlky@gmail.com>
2019-06-28 03:20:38 -0400

    Update index.php

--- first_try/index.php
+++ first_try/index.php
@@ -1,9 +1,11 @@
 <h1>Hello, php</h1>
 <p>
 <?php 
-echo "this is a test\n";
+echo "this is a test <br><br>";
 $haha = 10 + 1;
-echo "print a number '$haha'\n test done";
+echo "print a number '$haha'<br><br> test done<br><br>";
+$str = 'xixi';
+echo "print a string '&str'";
  ?>
 </p>
 <p>done</p>
