2b1a149546ffb06341a1441244e4ae36b81cbf32
kyle-luo <tsjlky@gmail.com>
2019-06-28 15:48:11 -0400

    Update index.php

--- first_try/index.php
+++ first_try/index.php
@@ -3,9 +3,38 @@
 <?php 
 echo "this is a test <br><br>";
 $haha = 10 + 1;
-echo "print a number '$haha'<br><br> test done<br><br>";
+echo "print a number '$haha'<br><br>";
 $str = 'xixi';
-echo "print a string '&str'";
+echo "print a string '$str'<br><br> ";
+
+$try = '44'.'33';
+echo "print conc '$try'<br><br> ";
+
+$haha++;
+echo "print number again with ++ '$haha' <br><br>";
  ?>
 </p>
+<h1>
+    Try tenary here
+</h1>
+<?php
+$a = 123;
+$msg = $a > 100 ? "Large" : "Small";
+echo "$msg<br>";
+$msg = $a == 0 ? "True" : "False";
+echo "$msg<br>";
+$msg = $a % 2 == 0 ? "Even" : "Odd";
+echo "$msg<br>";
+$msg = $a % 2 ? "Odd" : "Even";
+echo "$msg this uses conversion<br><br>";
+
+?>
+<h1>
+    This is Side-Effect
+</h1>
+<?php
+$out = "Hello";
+$out .= " PHP";
+echo $out.'<br>';
+?>
 <p>done</p>
