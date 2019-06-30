9a37f6edd763ab0c61fccc6583e3366cec6cedfb
kyle-luo <tsjlky@gmail.com>
2019-06-30 01:48:11 -0400

    HW3

---
+++ WA HW3/index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Suckway PHP</title>
+</head>
+<body>
+<h1>
+    Suckway PHP
+</h1>
+<pre>
+    "ASCII ART:
+     **   **
+    /**  **
+    /** **
+    /****
+    /**/**
+    /**//**
+    /** //**
+    //   //
+</pre>
+<?php
+$sha = hash('sha256', 'Suckway');
+echo "The SHA256 of my name is $sha";
+?>
+<br><br>
+<a href="fail.php">Click to get a fail :(</a><br><br>
+<a href="check.php">Click to check setting :)</a>
+</body>
+</html>
+
+
